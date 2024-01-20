42d6395143aaf5eeecddbdfb17ecd514280fc89d
Andres Perez <andresiitoo17@gmail.com>
2024-01-20 09:09:28 -0500

    Add new feature: age calc

--- welcome.py
+++ welcome.py
@@ -1,6 +1,9 @@
 name = "Andres"
 country = "Colombia"
+birthday = 2000
+now = 2024
 
 print("You're name", name, "and you're from", country)
 print("You're name", name)
 print("You're from", country)
+print("Your age is", now - birthday)
