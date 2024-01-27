432efb244e174f9d567ebe7016ddb2e46d5c36f4
Andres Perez <andresiitoo17@gmail.com>
2024-01-27 07:54:33 -0500

    Scripts de explicación de condicionales y funciones en Python

---
+++ condicional1.py
@@ -0,0 +1,20 @@
+#LOS CONDICIONALES
+'''
+Pueden ser simples o compuestos / anidados
+Es necesario usar tabla de operadores lógicos 
+> Mayor que
+< Menor que
+>= Mayor o igual que 
+<= Menor o igual que 
+<> Diferente de 
+= Asignación
+== Igual a (Operador de comparación)
+Se usan los valores o expresiones booleanas 
+'''
+n1 = 0
+if n1 > 0:
+    print("El número ", n1, "es positivo")
+elif n1 < 0:
+    print("El número ", n1, "es negativo")
+else:
+    print("El valor es cero")
